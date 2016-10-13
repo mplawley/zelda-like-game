f3915b299aff7c5aa882138a23a712765f8ef323
mplawley <mark.lawley@gmail.com>
2016-10-13 14:37:20 -0400

    Mage positioning and camera following

---
+++ Assets/Scripts/Mage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Mage : PT_MonoBehaviour //NOT MonoBehaviour
+{
+	public static Mage S;
+
+	void Awake()
+	{
+		S = this; //Mage Singleton
+	}
+}
